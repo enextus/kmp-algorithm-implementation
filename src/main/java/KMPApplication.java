5dc06e52455136520f78036263321915fc476cc6
Your Name <you@example.com>
2023-07-03 17:36:00 +0200

    app 008

--- src/main/java/KMPApplication.java
+++ src/main/java/KMPApplication.java
@@ -1,15 +1,29 @@
 import javax.swing.*;
-import java.awt.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class KMPApplication {
+    private static final int FRAME_WIDTH = 430;
+    private static final int FRAME_HEIGHT = 200;
+    private static final int TEXT_FIELD_LENGTH = 10;
+    private static final int X_LABEL_POSITION = 10;
+    private static final int Y_TEXT_LABEL_POSITION = 10;
+    private static final int Y_PATTERN_LABEL_POSITION = 40;
+    private static final int X_INPUT_POSITION = 100;
+    private static final int Y_BUTTON_POSITION = 80;
+    private static final int BUTTON_WIDTH = 80;
+    private static final int BUTTON_HEIGHT = 25;
+    private static final int X_RESULT_LABEL_POSITION = 100;
+    private static final int Y_RESULT_LABEL_POSITION = 80;
+    private static final int RESULT_LABEL_WIDTH = 300;
+    private static final int RESULT_LABEL_HEIGHT = 25;
 
     public KMPApplication() {
         KMP kmp = new KMP();
 
         JFrame frame = new JFrame("KMP Algorithm");
-        frame.setSize(400, 200);
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
@@ -24,27 +38,27 @@ public class KMPApplication {
         panel.setLayout(null);
 
         JLabel textLabel = new JLabel("Enter text:");
-        textLabel.setBounds(10, 10, 80, 25);
+        textLabel.setBounds(X_LABEL_POSITION, Y_TEXT_LABEL_POSITION, BUTTON_WIDTH, BUTTON_HEIGHT);
         panel.add(textLabel);
 
-        JTextField textInput = new JTextField(20);
-        textInput.setBounds(100, 10, 160, 25);
+        JTextField textInput = new JTextField(TEXT_FIELD_LENGTH);
+        textInput.setBounds(X_INPUT_POSITION, Y_TEXT_LABEL_POSITION, RESULT_LABEL_WIDTH, BUTTON_HEIGHT);
         panel.add(textInput);
 
         JLabel patternLabel = new JLabel("Enter pattern:");
-        patternLabel.setBounds(10, 40, 80, 25);
+        patternLabel.setBounds(X_LABEL_POSITION, Y_PATTERN_LABEL_POSITION, BUTTON_WIDTH, BUTTON_HEIGHT);
         panel.add(patternLabel);
 
-        JTextField patternInput = new JTextField(20);
-        patternInput.setBounds(100, 40, 160, 25);
+        JTextField patternInput = new JTextField(TEXT_FIELD_LENGTH);
+        patternInput.setBounds(X_INPUT_POSITION, Y_PATTERN_LABEL_POSITION, RESULT_LABEL_WIDTH, BUTTON_HEIGHT);
         panel.add(patternInput);
 
         JButton searchButton = new JButton("Search");
-        searchButton.setBounds(10, 80, 80, 25);
+        searchButton.setBounds(X_LABEL_POSITION, Y_BUTTON_POSITION, BUTTON_WIDTH, BUTTON_HEIGHT);
         panel.add(searchButton);
 
         JLabel resultLabel = new JLabel("");
-        resultLabel.setBounds(100, 80, 300, 25);
+        resultLabel.setBounds(X_RESULT_LABEL_POSITION, Y_RESULT_LABEL_POSITION, RESULT_LABEL_WIDTH, RESULT_LABEL_HEIGHT);
         panel.add(resultLabel);
 
         searchButton.addActionListener(new ActionListener() {
