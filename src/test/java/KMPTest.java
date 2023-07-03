34d5acebc9cd44e56a290afeb1a2428776c88bfc
Your Name <you@example.com>
2023-07-03 16:02:39 +0200

    app 002.6

--- src/test/java/KMPTest.java
+++ src/test/java/KMPTest.java
@@ -5,10 +5,28 @@ class KMPTest {
 
     @Test
     void computePrefixArrayTest() {
+
         KMP kmp = new KMP();
-        assertArrayEquals(new int[]{0, 0, 0, 1, 2, 3, 4, 2}, kmp.computePrefixArray("abcdabcda"));
+
         assertArrayEquals(new int[]{0, 0, 0, 0, 0}, kmp.computePrefixArray("abcde"));
         assertArrayEquals(new int[]{0, 1, 2, 3, 4}, kmp.computePrefixArray("aaaaa"));
+    }
+
+    @Test
+    public void OtherComputePrefixArrayTest() {
+
+        KMP kmp = new KMP();
+
+        String pattern = "abcdabcda";
+        int[] prefixArray = kmp.computePrefixArray(pattern);
+
+        for (int i = 0; i < pattern.length(); i++) {
+
+            String expectedPrefix = pattern.substring(0, prefixArray[i]);
+            String actualSuffix = pattern.substring(i - prefixArray[i] + 1, i + 1);
+
+            assertEquals(expectedPrefix, actualSuffix);
+        }
     }
 
     @Test
