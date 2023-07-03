2123e2f902fd0547de8bd6548080dc9ab26ec155
Your Name <you@example.com>
2023-07-03 17:50:14 +0200

    app 011

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,3 @@
-/*
-import java.util.Scanner;
-
-public class App {
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the main text: ");
-        String text = scanner.nextLine();
-
-        System.out.println("Enter the pattern to be searched: ");
-        String pattern = scanner.nextLine();
-
-        KMP kmp = new KMP();
-        boolean result = kmp.KMPAlgorithm(text, pattern);
-
-        if (result) System.out.println("Pattern found in the text");
-        else System.out.println("Pattern not found in the text");
-    }
-
-}
-*/
 import javax.swing.SwingUtilities;
 
 public class App {
