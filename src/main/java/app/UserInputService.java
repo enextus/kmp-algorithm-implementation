3d4a96484400de5a9eaf5841e73cfbbdf563ce13
Your Name <you@example.com>
2023-07-06 10:47:48 +0200

    app 4.8

--- src/main/java/app/UserInputService.java
+++ src/main/java/app/UserInputService.java
@@ -2,18 +2,40 @@ package app;
 
 public class UserInputService {
 
+    /**
+     * Validates the user input based on the application's requirements.
+     *
+     * @param userInput The input provided by the user.
+     * @return The validated user input if it's valid, otherwise returns null.
+     */
     public String getValidatedUserInput(String userInput) {
-        // Реализация метода
-        return null;
+        if (isInputAllowed(userInput)) {
+            return userInput;
+        } else {
+            System.out.println("Invalid input: " + userInput);
+            return null;
+        }
     }
 
+    /**
+     * Checks if the user input meets the application's requirements.
+     *
+     * @param userInput The input provided by the user.
+     * @return True if the input is allowed, otherwise returns false.
+     */
     public boolean isInputAllowed(String userInput) {
-        // Реализация метода
-        return false;
+        // An example of a simple validation could be checking if the input is not null or empty
+        return userInput != null && !userInput.trim().isEmpty();
     }
 
+    /**
+     * Processes the user input based on the application's logic.
+     *
+     * @param userInput The input provided by the user.
+     * @return The processed user input if it's valid, otherwise returns null.
+     */
     public String processUserInput(String userInput) {
-        // Реализация метода
-        return null;
+        // In this example, processing the input could mean simply validating it and returning the result
+        return getValidatedUserInput(userInput);
     }
 }
